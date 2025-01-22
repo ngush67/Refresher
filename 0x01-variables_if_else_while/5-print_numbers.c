34f492924062d17ba719d40be6ce43b988985c23
Ngush67 <olsonngula@gmail.com>
2025-01-22 23:27:07 +0300

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+int main(void)
+{
+	    int digit;
+
+	        for (digit = 0; digit < 10; digit++) // Loop through digits 0 to 9
+						     //     {
+						     //             putchar(digit + '0'); // Convert digit to its ASCII equivalent and print
+						     //                 }
+						     //                     putchar('\n'); // Print a newline character
+						     //
+						     //                         return (0);
+						     //                         }
+						     //
