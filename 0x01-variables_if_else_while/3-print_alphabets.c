dde3c3e01eab16da6bf9a7946bce7a704d1495e3
Ngush67 <olsonngula@gmail.com>
2025-01-22 23:18:50 +0300

    Alphabets printing

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+int main(void)
+{
+	    char letter;
+
+	        for (letter = 'a'; letter <= 'z'; letter++) // Loop through lowercase letters
+							    //     {
+							    //             putchar(letter); // Print each letter
+							    //                 }
+							    //                     putchar('\n'); // Print a newline character
+							    //
+							    //                         return (0);
+							    //                         }
+							    //
