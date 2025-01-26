74d77958674c1a11220c5b6d25834d992f39ae2c
Ngush67 <olsonngula@gmail.com>
2025-01-26 23:00:26 +0300

    Olly

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Description: Prints "_putchar" followed by a new line.
+ *     *
+ *      * Return: Always 0 (Success)
+ *       */
+int main(void)
+{
+	    char text[] = "_putchar";
+	        int i;
+
+		    for (i = 0; text[i] != '\0'; i++)
+			        {
+					        _putchar(text[i]);
+						    }
+		        _putchar('\n');
+
+			    return (0);
+}
+
