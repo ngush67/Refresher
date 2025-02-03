9a7a58e94cf10e720c23f6cb3cccbafe862d4449
Ngush67 <olsonngula@gmail.com>
2025-02-04 00:01:26 +0300

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,20 +1,13 @@
 #include <stdio.h>
 
-/**
- *  * main - Entry point
- *   * 
- *    * Return: Always 0 (Success)
- *     */
+
 int main(void)
 {
-	    int digit;
+	int i;
+	for (i=0; i < 10; i++)
+	{printf("%d",i);
+	}
+	printf("\n");
 
-	        for (digit = 0; digit < 10; digit++) // Loop through digits 0 to 9
-						     //     {
-						     //             putchar(digit + '0'); // Convert digit to its ASCII equivalent and print
-						     //                 }
-						     //                     putchar('\n'); // Print a newline character
-						     //
-						     //                         return (0);
-						     //                         }
-						     //
+	return (0);
+}
