c231f7fa1e4b904ef83f6ba315a1b46d52d4399e
Ngush67 <olsonngula@gmail.com>
2025-02-03 23:42:07 +0300

    Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,32 +2,28 @@
 #include <time.h>
 #include <stdio.h>
 
-/**
- *  * main - Entry point 
- *   *
- *    * Return: Always 0 (Success)
- *     */
+/* betty style doc for function main goes there */
 int main(void)
 {
 	    int n;
 
-	        srand(time(0)); // Seed the random number generator
-				//     n = rand() - RAND_MAX / 2; // Assign a random number to n
-				//
-				//         // Check if the number is positive, zero, or negative and print the result
-				//             if (n > 0)
-				//                 {
-				//                         printf("%d is positive\n", n);
-				//                             }
-				//                                 else if (n == 0)
-				//                                     {
-				//                                             printf("%d is zero\n", n);
-				//                                                 }
-				//                                                     else
-				//                                                         {
-				//                                                                 printf("%d is negative\n", n);
-				//                                                                     }
-				//
-				//                                                                         return (0);
-				//                                                                         }
-				//
+	        srand(time(0));
+		    n = rand() - RAND_MAX / 2;
+
+		        /* your code goes here */
+		        if (n > 0)
+				    {
+					            printf("%d is positive\n", n);
+						        }
+			    else if (n == 0)
+				        {
+						        printf("%d is zero\n", n);
+							    }
+			        else
+					    {
+						            printf("%d is negative\n", n);
+							        }
+
+				    return (0);
+}
+
