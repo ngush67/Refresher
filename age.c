c4ad929876f364f5a35fcdc11bc7217e1d31abfc
Ngush67 <olsonngula@gmail.com>
2025-02-12 21:25:11 +0300

    Oldhead

---
+++ age.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int age;
+    
+    // Asking for user input
+    printf("Enter your age: ");
+    scanf("%d", &age);
+    
+    // Displaying the entered age
+    printf("You are %d years old.\n", age);
+    
+    return 0;
+}
+
